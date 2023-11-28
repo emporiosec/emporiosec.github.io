2be922acd478815e5ca83c64d605e96267d2ea2a
Lucas Nolêto <emporiosecpnz@gmail.com>
2023-11-28 15:26:07 -0300

    90% full

---
+++ assets/js/site.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const sobreNosTexto = document.getElementById('sobre-nos-texto');
+    const lerMaisLink = document.getElementById('ler-mais-link');
+    const iconeLerMais = document.getElementById('icone-ler-mais');
+
+    lerMaisLink.addEventListener('click', function () {
+        sobreNosTexto.classList.toggle('exibir-texto');
+        if (sobreNosTexto.classList.contains('exibir-texto')) {
+            iconeLerMais.innerHTML = '&#9650;';
+        } else {
+            iconeLerMais.innerHTML = '&#9660;';
+        }
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+        document.getElementById("overlay").style.display = "none";
+    }, 2000);
+});
